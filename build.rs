3d895c7046e74b66b116f1b3f5b1eb48a5e548df
Serhii Plyhun <S.Plyhun@gentics.com>
2025-04-17 12:27:45 +0200

    Builds

--- build.rs
+++ build.rs
@@ -1,99 +1,72 @@
-// Copied from https://github.com/webview/webview_rust/blob/dev/webview-official-sys/build.rs
-
-use cc::Build;
+use cmake;
 use std::env;
 use std::fs;
-use std::path::PathBuf;
+use std::io;
+use std::path::{Path, PathBuf};
 
 fn main() {
-    let mut build = Build::new();
-
     let target = env::var("TARGET").unwrap();
 
+    let mut build = cmake::Config::new("webview-official");
     build
-        .cpp(true)
-        .include("webview-official/webview.h")
-        .flag_if_supported("/std:c++11")
-        .flag_if_supported("-w");
-
-    // if env::var("DEBUG").is_err() {
-    //     build.define("NDEBUG", None);
-    // } else {
-    //     build.define("DEBUG", None);
-    // }
-
+        .define("CMAKE_BUILD_TYPE", "Release")
+        .define("CMAKE_INSTALL_PREFIX", "webview")
+        .define("CMAKE_CXX_STANDARD", "17")
+        .define("CMAKE_C_STANDARD", "11")
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .define("BUILD_EXAMPLES", "OFF")
+        .define("BUILD_TESTING", "OFF")
+        .define("BUILD_GTK3", "ON")
+        .define("BUILD_GTK4", "OFF");
     if target.contains("windows") {
-        // build.define("UNICODE", None); // doesn't work atm.
         build
-            .file("webview-official/webview.cc")
-            .flag_if_supported("/std:c++17");
-        build.include("webview-official/script");
-
-        for &lib in &["windowsapp", "user32", "oleaut32", "ole32"] {
-            println!("cargo:rustc-link-lib={}", lib);
-        }
-
-        let webview2_arch = if target.contains("x86_64") {
-            "x64"
-        } else {
-            "x86"
-        };
-
-        // calculate full path to WebView2Loader.dll
-        let mut webview2_path_buf = PathBuf::from(env::current_dir().unwrap().to_str().unwrap());
-        webview2_path_buf
-            .push("webview-official/script/Microsoft.Web.WebView2.0.9.488/build/native");
-        webview2_path_buf.push(webview2_arch);
-        let webview2_dir = webview2_path_buf.as_path().to_str().unwrap();
-
-        let loader_asm_name = "WebView2Loader.dll";
-
-        println!("cargo:rustc-link-search={}", webview2_dir);
-        println!("cargo:rustc-link-lib={}", loader_asm_name);
-
-        // copy WebView2Loader.dll to `target/debug`
-        let mut src_asm_buf = PathBuf::from(webview2_dir);
-        src_asm_buf.push(loader_asm_name);
-
-        // we want to be able to calculate C:\crate\root\target\debug\
-        //           while we can get this ^^^^^^^^^^^^^   and  ^^^^^ from env::current_dir() and %PROFILE% respectively
-        // there's no way to get this (reliably)         ^^^^^^
-        // we can, however, use %OUT_DIR% (C:\crate\root\target\debug\build\webview_rust-xxxx\out\)
-        // and navigate backwards to here  ^^^^^^^^^^^^^^^^^^^^^^^^^^
-        let mut target_asm_buf = PathBuf::from(env::var("OUT_DIR").unwrap());
-        target_asm_buf.pop();
-        target_asm_buf.pop();
-        target_asm_buf.pop();
-        target_asm_buf.push(loader_asm_name);
-
-        fs::copy(src_asm_buf.as_path(), target_asm_buf.as_path()).unwrap();
+            .define("BUILD_WIN32", "ON")
+            .define("BUILD_WIN64", "OFF");
+    } else if target.contains("linux") {
+        build
+            .define("BUILD_WIN32", "OFF")
+            .define("BUILD_WIN64", "OFF");
     } else if target.contains("apple") {
-        build.file("webview-official/webview.cc").flag("-std=c++11");
-
-        println!("cargo:rustc-link-lib=framework=Cocoa");
-        println!("cargo:rustc-link-lib=framework=WebKit");
-    } else if target.contains("linux") || target.contains("bsd") {
-        let lib = pkg_config::Config::new()
-            .atleast_version("2.8")
-            .probe("webkit2gtk-4.0")
-            .unwrap();
-
-        for path in lib.include_paths {
-            build.include(path);
-        }
-        // pkg_config::Config::new()
-        //     .atleast_version("3.0")
-        //     .probe("gtk+-3.0")
-        //     .unwrap();
-
-        build.file("webview-official/webview.cc");
+        build
+            .define("BUILD_WIN32", "OFF")
+            .define("BUILD_WIN64", "OFF");
     } else {
-        panic!("Unsupported platform");
+        panic!("Unsupported target: {}", target);
     }
-
-    println!("cargo:rerun-if-changed=webview-official/webview.h");
-    println!("cargo:rerun-if-changed=webview-official/webview.cc");
-
-    build.compile("webview");
+    let dst = build.build();
+    let lib_dir = dst.join("build").join("webview").join("lib");
+    let include_dir = dst.join("build").join("webview").join("include");
+    let lib_path = lib_dir.join("libwebview.a");
+    let include_path = include_dir.join("webview");
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_path = PathBuf::from(&out_dir);
+    let out_include_path = out_path.join("include");
+    let out_lib_path = out_path.join("lib");
+    println!("cargo:rustc-link-search=native={}", out_lib_path.display());
+    println!("cargo:rustc-link-search=native={}", lib_dir.display());
+    println!("cargo:rustc-link-search=native={}", include_dir.display());
+    println!("cargo:rustc-link-search=native={}", out_include_path.display());
+    fs::create_dir_all(&out_include_path).unwrap();
+    fs::create_dir_all(&out_lib_path).unwrap();
+    fs::copy(&lib_path, out_lib_path.join("libwebview.a")).unwrap();
+    copy_recursively(&include_path, out_include_path.join("webview")).unwrap();
+    println!("cargo:rustc-link-lib=static=webview");
+    println!("cargo:rustc-link-search=native={}", out_lib_path.display());
+    println!("cargo:rustc-link-search=native={}", lib_dir.display());
+    println!("cargo:rustc-link-search=native={}", include_dir.display());
+    println!("cargo:rustc-link-search=native={}", out_include_path.display());
 }
 
+fn copy_recursively<P: AsRef<Path>, Q: AsRef<Path>>(source: P, destination: Q) -> io::Result<()> {
+    fs::create_dir_all(destination.as_ref())?;
+    for entry in fs::read_dir(source.as_ref())? {
+        let entry = entry?;
+        let filetype = entry.file_type()?;
+        if filetype.is_dir() {
+            copy_recursively(entry.path(), destination.as_ref().join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), destination.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
+}
